0834b50b7d96446b36447d6823d2f31f74d56bbe
Vansh856 <vanshshan5@gmail.com>
2026-07-01 19:46:20 +0530

    spreated the before login and after login sidebar using visibality

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import streamlit as st
 
-# 1. Define the pages
-landing_page = st.Page("views/landing.py", title="Home", icon="🏠", default=True)
-auth_page = st.Page("views/auth.py", title="Login / Register", icon="🔐")
+#==========================================================================
+#1. CHECKING THE USER'S AUTHENTICATION STATE WHEN THE APP LOADS
+#===========================================================================
 
-# The 1% Trick: Hide the dashboard from the sidebar using visibility=False [2]
-dashboard_page = st.Page("views/dashboard.py", title="Dashboard", icon="🏦", visibility="hidden")
+is_logged_in="authenticated_user" in st.session_state
+
+#=============================================
+# 2. DEFINING THE PAGES
+#=============================================
+
+landing_page = st.Page("views/landing.py", title="Home", icon="🏠", default=True)
+#changing the visibility according to the session state
+auth_page = st.Page("views/auth.py", title="Login / Register", icon="🔐",visibility="hidden" if is_logged_in else "visible")
+dashboard_page = st.Page("views/dashboard.py", title="Dashboard", icon="🏦", visibility="visible"if is_logged_in else "hidden")
 
 contact_page=st.Page("views/contact.py",title="contact",visibility="hidden")
 about_page=st.Page("views/about.py",title="About us",visibility="hidden")
-# 2. Setup the Navigation
+
+#=================================
+# 3. SETUP THE NAVIGATION
+#=================================
+
 pg = st.navigation([landing_page, auth_page, dashboard_page,contact_page,about_page])
 
-# 3. Run the App
+#========================
+# 4. run the app
+#========================
+
 pg.run()
